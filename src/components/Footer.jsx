d8eb94482b69367cd772a9b818416ae8c2cba55d
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-03-02 21:33:00 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -106,7 +106,7 @@ const Footer = () => {
       {/* Footer Bottom Bar */}
       <div className="footer-legal-bar">
         <div className="container bar-flex">
-          <p>&copy; {currentYear} ByteFlow Ltd. All Rights Reserved.</p>
+           <p>&copy; {currentYear} ByteFlow Ltd. All Rights Reserved. | Developed by <a href="https://aline-site-seven.vercel.app/"><span class="developer-name">Umugwaneza Aline</span></a></p>
           <div className="legal-links">
             <Link to="/privacy">Privacy Policy</Link>
             <span className="dot"></span>
